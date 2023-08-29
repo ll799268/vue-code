3a2220c134f5cfdc9dc04dea40279a9618e7bbfd
ll799268 <1010023095@qq.com>
2023-08-29 18:45:19 +0800

    init project

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  // 输出错误源码
   devtool: 'source-map',
   resolve: {
+    // 定义引入模块路径(按顺序执行)
     modules: [
+      // 先找根目录
       path.resolve(__dirname, ''), 
       path.resolve(__dirname, 'node_modules')
     ]
